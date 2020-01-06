c51229d364bd56ddeb805ddd1d6e0fab83c1dbe3
Ruslan Senatorov <55090151+Senatorov1@users.noreply.github.com>
2020-01-06 08:35:33 +0300

    Update randomeInsultMathFloor.js

--- randomeInsultMath.floor/randomeInsultMathFloor.js
+++ randomeInsultMath.floor/randomeInsultMathFloor.js
@@ -2,11 +2,13 @@ var randomeBodyParts = ["eye", "nose", "skull"];
 var randomeAdjectives = ["stupid", "idiotic"];
 var randomeWords = ["mouse"];
 //Выбор случайной части тела из массива randomeBodyRarts
-var randomeBodyPart = randomeBodyParts[Math.floor(Math.random() * 3)];
+var randomeBodyPart = randomeBodyParts[Math.floor(Math.random() * randomeBodyParts.length)];
 //Выбор прилагательного randomeAdjectives
-var randomeAdjective = randomeAdjectives[Math.floor(Math.random() * 2)];
+var randomeAdjective = randomeAdjectives[Math.floor(Math.random() * randomeAdjectives.length)];
 //Выбор слова randomeWords
-var randomeWord = randomeWords[Math.floor(Math.random() * 1)];
+var randomeWord = randomeWords[Math.floor(Math.random() * randomeWords.length)];
 //Соединяем случайные строчки в предложение 
 var randomInsult = "У тебя " + randomeBodyPart + " словно " + randomeAdjective + " " + randomeWord + "!!!";
 randomInsult;
+/*randomWords.length как множителя означает,
+что нам не понадобится менять этот код, если длина массива изменится.*/
