41936b16d1506a3dc2630e4acf8b6ba0bb7aed62
Ruslan Senatorov <55090151+Senatorov1@users.noreply.github.com>
2020-01-07 09:27:08 +0300

    Update objectCreate.js

--- objectCreate/objectCreate.js
+++ objectCreate/objectCreate.js
@@ -1,7 +1,8 @@
 var dino = [
     {name: "Rex", key1: "Putinozavr"},
-    {name: "lol", key1: "dovno"},
+    {rom: "lol", key1: "dovno"},
     {name: "trol", key1: "chaz"}
 ];
-dino[0]["name"];
+dino[1].key1;
+
 
