0680d8dcd1d47be57d813d37e0af69a49196746f
Ruslan Senatorov <55090151+Senatorov1@users.noreply.github.com>
2020-01-10 18:19:10 +0300

    Create moviesProgramm.js

---
+++ moviesProgramm/moviesProgramm.js
@@ -0,0 +1,19 @@
+var movies = {
+    "V poiskax": 
+    {
+        releaseDate:2003,
+        duration: 100,
+        actors: ["Albert", "elen"],
+        format: "Dvd"
+},
+
+"Test": 
+{
+    releaseDate:2003,
+    dutratuon: 132,
+    actors:["Roma","Elen"],
+    format:"Mp3"
+}
+};
+
+movies;
