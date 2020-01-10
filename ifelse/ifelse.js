53646c32a718e31709cf2d011c04827522f4356e
Ruslan Senatorov <55090151+Senatorov1@users.noreply.github.com>
2020-01-10 22:50:49 +0300

    +

---
+++ ifelse/ifelse.js
@@ -0,0 +1,10 @@
+var lemovChicen = true;
+var beefWithBlackBean = true;
+var sweeAndSourPork = true;
+if (lemovChicen) {
+    alert("YES");
+} else if (beefWithBlackBean) {
+    alert("YES-NO");
+} else {
+    alert("GO HOME");
+}
