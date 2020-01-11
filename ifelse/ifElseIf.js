d2e19c8fb1e1f84a0dd2bd9c16e84f3e4361632f
Ruslan Senatorov <55090151+Senatorov1@users.noreply.github.com>
2020-01-11 09:57:22 +0300

    Create ifElseIf.js

---
+++ ifelse/ifElseIf.js
@@ -0,0 +1,10 @@
+var condition1 = false;
+if (condition1) {
+    alert("do it catch");
+} else if (condition2) {
+    alert("do it catch2");
+} else if (condition3) {
+    alert("do it catch3");
+} else {
+    alert("otherwise do it");
+}
