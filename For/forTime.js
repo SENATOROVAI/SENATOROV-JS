aee3e7fb614ebc421e677ce2ae0a6b5181ed0b52
Ruslan Senatorov <55090151+Senatorov1@users.noreply.github.com>
2020-01-12 10:22:19 +0300

    Update forTime.js

--- For/forTime.js
+++ For/forTime.js
@@ -1,4 +1,4 @@
-var timeToSayHi = 3;
-for (i = 0; i < timeToSayHi; i++) {
-    alert("Hi");
+var animals = ["lion", "pig", "cat"];
+for (var i = 0; i < animals.length; i++) {
+    alert("in the zoo there - " + animals[i])
 }
