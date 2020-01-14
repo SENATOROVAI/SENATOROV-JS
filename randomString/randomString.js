e3446f36c5554c1465c75e5377f18351bd325bea
Ruslan Senatorov <55090151+Senatorov1@users.noreply.github.com>
2020-01-15 00:34:32 +0300

    Update randomString.js

--- randomString/randomString.js
+++ randomString/randomString.js
@@ -1,7 +1,12 @@
-var alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
-var empty = "";
-while (empty.length < 6){
-    empty+= alphabet[Math.floor(Math.random()*alphabet.length)];
-    console.log(empty);    
-}
+var input = "javascript is awesome";
+var output = "";
+for (var i = 0; i < input.length; i++){
+if (input[i]=="a") {output+="4"} else {
+ if (input[i]=="e") {output+="3"} else {
+if (input[i]=="i") {output+="1"} else {
+if (input[i]=="o") {output+="0"} else{output+=input[i]}
+ }}
+ }
+ }
 
+console.log(output);
