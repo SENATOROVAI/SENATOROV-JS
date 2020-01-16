df0ae0489689aa6739453fdf5c7af854af8aa63b
Ruslan Senatorov <55090151+Senatorov1@users.noreply.github.com>
2020-01-16 22:07:38 +0300

    Update myFirstGame.js

--- myFirstGame/myFirstGame.js
+++ myFirstGame/myFirstGame.js
@@ -1,13 +1,41 @@
-var words = [
+//создаём массив со словами
+let words = [
     "toque",
     "lion",
     "cat",
     "fish"
 ];
-var word = words[Math.floor(Math.random() * words.length)];
-var answerArray = [];
-for (var i = 0; i < word.length; i++){
+// выбираем случайное слово
+let word = words[Math.floor(Math.random() * words.length)];
+// создаём итоговый массив
+let answerArray = [];
+for (let i = 0; i < word.length; i++){
     answerArray[i] = "_";
 }
-var remaingLetters = word.length;
+let remaingLetters = word.length;
+// игровой цикл
+while (remaingLetters > 0) {
+    // показываем состояние игры
+    alert(answerArray.join(" "));
+// запрашиваем вариант ответа
+let guess = prompt("Угадайте букву");
+if (guess === null) {
+    //выходи из игрового цикла
+ break;
+} else if (guess.length !==1) {
+    alert("Пожалуйста, введите только одну букву.");
+} else {
+    // обновляем состояние игры
+    for (let j = 0; j < word.length; j++) {
+        if (word[j] === guess) {
+            answerArray[j] = guess;
+            remaingLetters--;
+        }
+        }
+    }
 
+}
+            // конец игрового цикла
+            alert(answerArray.join(" "));
+            //поздравляем игрока
+            alert("Coll guys " + word);
