d40f3a0afb10fb3e3d0c23f99e8fdb2d21c6bcee
Ruslan Senatorov <55090151+Senatorov1@users.noreply.github.com>
2020-01-18 22:48:16 +0300

    Create functionProgram.js

---
+++ function/functionProgram.js
@@ -0,0 +1,7 @@
+let pickRandomWord = function (words) {
+    return words[Math.floor(Math.random() * words.length)];
+};
+
+let randomWords = ["Планета", "Червяк", "Цветок", "Компьютер"];
+
+pickRandomWord(randomWords);
