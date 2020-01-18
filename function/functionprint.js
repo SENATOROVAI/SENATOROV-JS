9cab9ffa06a1b6eaab4b4e74d6032ba86179e52e
Ruslan Senatorov <55090151+Senatorov1@users.noreply.github.com>
2020-01-18 22:31:55 +0300

    Create functionprint.js

---
+++ function/functionprint.js
@@ -0,0 +1,6 @@
+let printMultipleTimes = function (howManyTimes, whatToDraw) {
+    for (let i = 0; i < howManyTimes; i++) {
+        alert(i + " " + whatToDraw);
+    }
+    };
+    printMultipleTimes(5, ".i.");
