a5744a669810620cf232fd96c0d61d33f495c154
Ruslan Senatorov <55090151+Senatorov1@users.noreply.github.com>
2020-01-20 01:19:39 +0300

    Update functionExercises.js

--- Exercises/functionExercises.js
+++ Exercises/functionExercises.js
@@ -1,16 +1,16 @@
-function areArraysSame(a,b){
-    for(let i = 0; i < a.length;i++){
-      if(a.length != b.length){
-         alert('Массивы разной длины');
-         break;
-       }
-       if(a[i] != b[i]){
-          alert('Массивы не совпадают');
-          return;
-       }
-       if(a[i] === b[i]){
-          alert('Массивы совпадают');
-       }
+function areArraysSame(a, b) {
+    for(let i = 0; i < a.length; i++){
+        if (a.length != b.length) {
+            alert("array false");
+            break;
+        }
+        if (a[i] != b[i]) {
+            alert("Array False");
+            return;
+        }
+        if (a[i] === b[i]) {
+            alert("array true");
+        }
     }
-  }
-   areArraysSame([1,2,2],[1,2,2]);
+}
+areArraysSame([1,2,2],[1,2,2]);
