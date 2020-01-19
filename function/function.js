4f1d51f27ff34ea288dc7aa26160cf8f18131430
Ruslan Senatorov <55090151+Senatorov1@users.noreply.github.com>
2020-01-20 00:10:38 +0300

    Update function.js

--- function/function.js
+++ function/function.js
@@ -1,6 +1,13 @@
-let fifthLetter = function (name) {
-    if (name.length < 5) {
-        return;
+let medalForScore = function (score) {
+    if (score < 3) {
+        return "test1";
     }
-    return name[4];
+    if (score < 5) {
+        return "test2";
+    }
+    if (score < 1) {
+        return "test3";
+    }
+    
+        return "test4";
 };
