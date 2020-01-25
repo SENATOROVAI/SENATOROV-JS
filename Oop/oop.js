1cdfbffbe6da995e482f56efb472761e0acb50e2
Ruslan Senatorov <55090151+Senatorov1@users.noreply.github.com>
2020-01-25 23:41:12 +0300

    Update oop.js

--- Oop/oop.js
+++ Oop/oop.js
@@ -1,3 +1,14 @@
+let speak = function () {
+    console.log(this.sound + " my name " + this.name);
+}
+let cat = {
+    sound: "mya",
+    name: "lol",
+    speak:speak
+};
+cat.speak();
+
+/*
 let dog = {
     name: "olady",
     legs: 4,
@@ -11,3 +22,4 @@ dog.bark = function () {
     console.log("gav-gav my name:" + this.name);
 };
 dog.bark();
+*/
